357e2fc7c9ec961a8e3bf84689ac2a49e8deef9c
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2023-01-02 15:10:00 +0300

    feat: added new component Skills

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,18 +1,14 @@
 import styles from './Main.module.scss'
+import Skills from "./Skills/Skills";
+import {Intro} from "./Intro/Intro";
 
 const Main = () => {
     return (
         <main className={styles.main}>
-            <div className={`container ${styles.container}`}>
-                <section className={styles.intro}>
-                    <span>Hi, there!</span>
-                    <h1>My name Ilya Rudakov.</h1>
-                    <p>I'm frontend developer</p>
-                </section>
-                <div className={styles.photo}>
-                    <img src="" alt=""/>
-                </div>
-            </div>
+            <Intro/>
+
+            <Skills/>
+
         </main>
     );
 };
--- src/components/main/skills/Skills.tsx
+++ src/components/main/skills/Skills.tsx
@@ -1,11 +1,63 @@
 import React from 'react';
 import styles from './Skills.module.scss'
+import Skill from "./Skill/Skill";
 
 const Skills = () => {
-    return (
-        <div className={styles.skills}>
 
-        </div>
+    const skills = [
+        {
+            skillID: 1,
+            title: 'JS',
+            img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias amet fugit natus numquam, perspiciatis!'
+        },
+        {
+            skillID: 2,
+            title: 'CSS',
+            img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias amet fugit natus numquam, perspiciatis!'
+        },
+        {
+            skillID: 3,
+            title: 'HTML',
+            img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias amet fugit natus numquam, perspiciatis!'
+        },
+        {
+            skillID: 4,
+            title: 'React',
+            img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias amet fugit natus numquam, perspiciatis!'
+        },
+        {
+            skillID: 5,
+            title: 'Redux',
+            img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias amet fugit natus numquam, perspiciatis!'
+        },
+        {
+            skillID: 6,
+            title: 'Formik',
+            img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias amet fugit natus numquam, perspiciatis!'
+        },
+    ]
+
+    const mappedSkills = skills.map(skill => {
+        return (
+            <Skill key={skill.skillID} skill={skill}/>
+        )
+    })
+
+    return (
+        <section className={styles.skills}>
+            <div className={`container`}>
+                <h2 className={styles.title}>My Skills</h2>
+                <ul className={styles.list}>
+                    {mappedSkills}
+                </ul>
+            </div>
+        </section>
     );
 };
 
