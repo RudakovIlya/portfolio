cd85e95fefab3c541dcb15aba92a3b1168d9ff39
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2023-01-02 18:24:46 +0300

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import Footer from "./components/Footer/Footer";
 
 function App() {
     return (
         <>
+            <Header/>
+            <Main/>
+            <Footer/>
 
         </>
     );
