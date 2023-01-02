a6537d12f6604523e86c79e12f5bd88ee779e17c
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2023-01-02 18:33:49 +0300

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
