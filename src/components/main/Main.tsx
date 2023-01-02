9deb3f2c3d78a61efeb56ead5213e75cb938123a
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2023-01-02 17:41:34 +0300

    feat: added new component Works

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,14 +1,14 @@
 import styles from './Main.module.scss'
 import Skills from "./Skills/Skills";
 import {Intro} from "./Intro/Intro";
+import {Works} from "./Works/Works";
 
 const Main = () => {
     return (
         <main className={styles.main}>
             <Intro/>
-
             <Skills/>
-
+            <Works/>
         </main>
     );
 };
